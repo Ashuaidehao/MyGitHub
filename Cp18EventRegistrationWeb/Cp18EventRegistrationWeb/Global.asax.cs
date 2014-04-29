87334ecda0580c345032be733f0a2456bad6efb9
Ashuaidehao <bfshm@qq.com>
2014-04-29 20:21:55 +0800

    Site.css

--- Cp18EventRegistrationWeb/Cp18EventRegistrationWeb/Global.asax.cs
+++ Cp18EventRegistrationWeb/Cp18EventRegistrationWeb/Global.asax.cs
@@ -11,6 +11,20 @@ namespace Cp18EventRegistrationWeb
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            //程序开始时调用，初始化一个“myTest”的ApplicationState——可以被任意用户读取
+            Application["myTest"] = 0;
+            Application["userCount"] = 0;
+        }
+
+        protected void Session_Start(object sender,EventArgs e)
+        {
+            //每个Session开始时调用，初始化一个“myTest”的SessionState——只能被本Session用户读取
+            Session["myTest"] = 0;
+
+            //读写全局变量注意锁定，否则会出现线程问题！
+            Application.Lock();
+            Application["userCount"] = (int) Application["userCount"] + 1;
+            Application.UnLock();
         }
     }
 }
