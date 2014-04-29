a3399fa210732c99ce3c94d29536b430b65412ea
Ashuaidehao <bfshm@qq.com>
2014-04-29 20:48:35 +0800

    MasterPage

---
+++ Cp18EventRegistrationWeb/Cp18EventRegistrationWeb/Events.Master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cp18EventRegistrationWeb
+{
+    public partial class Events : System.Web.UI.MasterPage
+    {
+        protected void Page_Load (object sender, EventArgs e)
+        {
+
+        }
+
+
+        void Page_PreInit(object sender,EventArgs e)
+        {
+            if(Request.UserAgent.Contains("MSIE"))
+            {
+                this.MasterPageFile = "~/IE.master";
+            }
+            else
+            {
+                this.MasterPageFile = "~/Default.master";
+            }
+        }
+    }
+}
